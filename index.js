3e876611a8b9ee8d51c8df22136972dac745199e
Noor Mohammad <noormohammad@AAOMAN.COM>
2023-01-17 15:50:46 +0400

    amazon products data added

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
-app.use(cors())
-const {readdirSync} = require('fs')
-
+app.use(cors());
+const { readdirSync } = require("fs");
 
 // =============== Dynamic Routes Start here ==================
-readdirSync("./routes").map((file)=> app.use("/", require("./routes/"+file)))
+readdirSync("./routes").map((file) =>
+  app.use("/", require("./routes/" + file))
+);
 // =============== Dynamic Routes End here ====================
+// =============== Home Routes Start here =====================
+app.get("/", (req, res) => {
+  res.send(
+    "You are now in fakeStoreApi server. Route through pages to get your data!"
+  );
+});
+// =============== Home Routes End here =======================
 
-require('dotenv').config()
-const port =process.env.PORT || 8000
+require("dotenv").config();
+const port = process.env.PORT || 8000;
 
-
-
-app.listen(port,()=>{
-    console.log(`Server is running on ${port}`)
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
